60d29ff55c2c74c205ccfbcd07a3b3c622f64fc8
Judah Paul <judahpaul16@gmail.com>
2024-04-29 16:02:14 -0400

    MacOS executable is redundant, removing

--- build-scripts/build.bat
+++ build-scripts/build.bat
@@ -3,7 +3,7 @@
 REM Set GOOS and GOARCH for Windows
 SET GOOS=windows
 SET GOARCH=amd64
-go build -o dist/clear-badge-cache-win.exe
+go build -o dist/clear-badge-cache.exe
 
 REM Optional: Reset environment variables (not strictly necessary)
 SET GOOS=
@@ -12,13 +12,9 @@ SET GOARCH=
 REM Set GOOS and GOARCH for Linux
 SET GOOS=linux
 SET GOARCH=amd64
-go build -o dist/clear-badge-cache-linux.sh
+go build -o dist/clear-badge-cache.sh
 
 REM Optional: Reset environment variables (not strictly necessary)
 SET GOOS=
 SET GOARCH=
 
-REM Set GOOS and GOARCH for macOS
-SET GOOS=darwin
-SET GOARCH=amd64
-go build -o dist/clear-badge-cache-mac.sh
